4540dfa29fc540ac03b302daf59796cdf47ac68a
nou <nou13n8e@gmail.com>
2023-07-06 16:32:20 +0900

    회원가입 : 실명확인 처리 수정

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -14,9 +14,6 @@ agreeno?.addEventListener('click',()=>{
 });
 
 // check
-let name2=document.querySelector("#name2");
-let secret1=document.querySelector("#secret1");
-let secret2=document.querySelector("#secret2");
 let check=document.querySelector("#check");
 let checkok2=document.querySelector("#checkok2");
 let checkno2=document.querySelector("#checkno2");
@@ -26,7 +23,7 @@ checkok2?.addEventListener('click', ()=>{
 
     if(checkfrm2.name2.value == "") alert("이름을 입력해주세요.");
     else if(checkfrm2.secret1.value == "") alert("주민등록번호를 입력해주세요.");
-    else if(checkfrm2.secret2.value == "") alert("주민등록번호를 입력해주세요.");
+    else if(checkfrm2.secret2.value == "") alert("주민등록번호를 더 입력해주세요.");
     else if(!check.checked) alert("주민등록번호 처리에 동의해주세요.");
     else {
         // let param= "?name=" + checkfrm2.name2.value;
