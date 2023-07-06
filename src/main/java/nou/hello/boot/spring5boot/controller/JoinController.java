650690489071a25a417feb31bb6c35b8e41e82fc
nou <nou13n8e@gmail.com>
2023-07-06 15:51:00 +0900

    회원가입 : 실명확인 처리

--- src/main/java/nou/hello/boot/spring5boot/controller/JoinController.java
+++ src/main/java/nou/hello/boot/spring5boot/controller/JoinController.java
@@ -1,10 +1,14 @@
 package nou.hello.boot.spring5boot.controller;
 
+import nou.hello.boot.spring5boot.model.Check;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/join")
@@ -21,6 +25,19 @@ public class JoinController {
         logger.info("join check 호출!");
         return "/join/check";
     }
+    @PostMapping("/check")
+    public String checkok(Check check, HttpSession sess) {
+        logger.info("join checkok 호출!");
+        // check에서 보낸 개인정보를 세션에 저장하고 join으로 이동
+        String viewPage="redirect:/join/check";
+
+        if(check.getName2() != null) {
+            sess.setAttribute("check", check);
+            viewPage="redirect:/join/join";
+        }
+        return viewPage;
+    }
+
     @GetMapping("/join")
     public String join() {
         logger.info("join join 호출!");
