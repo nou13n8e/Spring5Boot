a416e06ffc1dfb42f7dd3699f66ed8b2dcffa014
nou <nou13n8e@gmail.com>
2023-07-06 15:50:33 +0900

    회원가입 : 실명확인 정보 저장하기 위해 클래스 생성

---
+++ src/main/java/nou/hello/boot/spring5boot/model/Check.java
@@ -0,0 +1,14 @@
+package nou.hello.boot.spring5boot.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Check {
+    private String name2;
+    private String secret1;
+    private String secret2;
+}
