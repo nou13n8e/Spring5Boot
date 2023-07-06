1187f770681df2b5f198165d96029a7c158dced9
nou <nou13n8e@gmail.com>
2023-07-06 15:33:50 +0900

    회원가입 : 자바스크립트 추가 check

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -13,3 +13,28 @@ agreeno?.addEventListener('click',()=>{
     location.href="/";
 });
 
+// check
+let name2=document.querySelector("#name2");
+let secret1=document.querySelector("#secret1");
+let secret2=document.querySelector("#secret2");
+let check=document.querySelector("#check");
+let checkok2=document.querySelector("#checkok2");
+let checkno2=document.querySelector("#checkno2");
+
+checkok2?.addEventListener('click', ()=>{
+    let checkfrm2 = document.forms.checkfrm2;
+
+    if(checkfrm2.name2.value == "") alert("이름을 입력해주세요.");
+    else if(checkfrm2.secret1.value == "") alert("주민등록번호를 입력해주세요.");
+    else if(checkfrm2.secret2.value == "") alert("주민등록번호를 입력해주세요.");
+    else if(!check.checked) alert("주민등록번호 처리에 동의해주세요.");
+    else {
+        let param= "?name=" + checkfrm2.name2.value;
+        param += "&secret1=" + checkfrm2.secret1.value;
+        param += "&secret2=" + checkfrm2.secret2.value;
+        location.href="/join/join" + param;
+    }
+});
+checkno2?.addEventListener('click', ()=>{
+    location.href="/"
+});
