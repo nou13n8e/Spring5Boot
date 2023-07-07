6af91cb54cba18480c8a29b1532c93f755063b15
nou <nou13n8e@gmail.com>
2023-07-07 11:56:16 +0900

    회원가입 : MemberDAO 단위 테스트(성공)

--- src/test/java/nou/hello/boot/spring5boot/MemberDAOUnitTest.java
+++ src/test/java/nou/hello/boot/spring5boot/MemberDAOUnitTest.java
@@ -9,12 +9,9 @@ import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.junit.runner.RunWith;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(MemberDAOImpl.class)
@@ -25,8 +22,8 @@ public class MemberDAOUnitTest {
     @Test
     @DisplayName("MemberDAO insert Test")
     void insertMember() {
-        Member m=new Member(null, "abc123", "987xyz", "abc123", "abc123@987xyz.com",
-                "123-456", "서울특별시 관악구", "남부순환로 1820", "010-1234-5678", null);
+        Member m=new Member(null, "", "", "", "",
+                "", "", "", "", null);
         int result=mdao.insertMember(m);
         System.out.println(result);
         assertEquals(result, 1);
