07ad710948861d5e6f62896408eef263465a48bc
nou <nou13n8e@gmail.com>
2023-07-07 16:22:00 +0900

    우편번호 조회 : Zipcode 클래스 추가

---
+++ src/main/java/nou/hello/boot/spring5boot/model/Zipcode.java
@@ -0,0 +1,17 @@
+package nou.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Zipcode {
+    private String zipcode;
+    private String sido;
+    private String gugun;
+    private String dong;
+    private String ri;
+    private String bunji;
+}
