8aa853a941d0ee51bfc667c7e3ebf4872c327c94
nou <nou13n8e@gmail.com>
2023-07-07 16:22:50 +0900

    우편번호 조회 : mybatis findZipcode mapping 추가

--- src/main/java/nou/hello/boot/spring5boot/mybatis/MemberMapper.java
+++ src/main/java/nou/hello/boot/spring5boot/mybatis/MemberMapper.java
@@ -1,6 +1,7 @@
 package nou.hello.boot.spring5boot.mybatis;
 
 import nou.hello.boot.spring5boot.model.Member;
+import nou.hello.boot.spring5boot.model.Zipcode;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface MemberMapper {
     int insertMember(Member m);
     List<Member> selectMember();
+
+    List<Zipcode> findZipcode(String dong);
 }
