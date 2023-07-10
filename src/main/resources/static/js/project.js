03d9067818a59335d7309f0729517a34aaca129f
nou <nou13n8e@gmail.com>
2023-07-10 12:00:22 +0900

    우편번호 조회 : 우편번호 검색 모달 띄우기

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -37,3 +37,63 @@ checkok2?.addEventListener('click', ()=>{
 checkno2?.addEventListener('click', ()=>{
     location.href="/"
 });
+
+
+
+// join
+let fzipbtn=document.querySelector("#find");
+let zipbtn=document.querySelector("#zipbtn");
+let dong=document.querySelector("#dong");
+let zipmodal=document.querySelector("#zipmodal")
+let addrlist=document.querySelector("#addrlist");
+let sendzip=document.querySelector("#send");
+let modal=null;
+zipbtn?.addEventListener('click', ()=>{
+    while(addrlist.lastChild) {
+        addrlist.removeChild(addrlist.lastChild);
+        // removeChild 하위 요소만 지우는 기능
+        // 이전 검색 결과 지우기
+    }
+    dong.value="";  // 이전 검색 키워드 지우기
+
+    try {
+        // 새로운 모달 생성
+        modal=new bootstrap.Modal(zipmodal, {});
+    } catch (e) { }
+        modal.show();
+});
+const showzipaddr = (jsons) => {
+    jsons=JSON.parse(jsons); //문자열을 json객체로 변환
+    let addr="";
+    jsons.forEach(function (data, idx) {
+       addr+=`<option>${data['zipcode']} ${data['sido']} ${data['gugun']}
+                ${data['dong']} ${data['bunji']}</option>`;
+    });
+    addrlist.innerHTML=addr;
+};
+fzipbtn?.addEventListener('click', ()=>{
+    if(dong.value==='') {
+        alert("동 이름을 입력하세요.");
+        return;
+    }
+    const url="/join/zipcode?dong="+dong.value;
+    fetch(url).then(response=>response.text())
+        .then(text=>showzipaddr(text));
+});
+
+
+sendzip?.addEventListener('click', ()=>{
+    let frm=document.forms.joinfrm1;
+    let addr=addrlist.value;    // 선택한 주소 항목
+    if(addr != "") {
+        let zip=addr.split(" ")[0];
+        let vaddr=`${addr.split(" ")[1]} ${addr.split(" ")[2]} ${addr.split(" ")[3]}`;
+        frm.zip1.value=zip.value("-")[0];
+        frm.zip2.value=zip.value("-")[1];
+        frm.addr1.value=vaddr;
+
+        modal.hide();
+    } else {
+        alert("주소를 선택하세요.")
+    }
+});
