08c87d508ce42b3e3b9b2dfe1f23ad021149be6d
nou <nou13n8e@gmail.com>
2023-07-11 13:50:29 +0900

    로그아웃 : logout 구현

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -203,7 +203,7 @@ let lgnuid=document.querySelector("#userid");
 let lgnpwd=document.querySelector("#passwd");
 let lgnfrm=document.querySelector("#lgnfrm");
 loginbtn?.addEventListener('click', ()=>{
-    if(lgnuid.value===""){
+    if(lgnuid.value==="") {
         alert("아이디를 입력해주세요.")
     } else if (lgnpwd.value==="") {
         alert("비밀번호를 입력해주세요.")
@@ -212,4 +212,10 @@ loginbtn?.addEventListener('click', ()=>{
         lgnfrm.action="/join/login";
         lgnfrm.submit();
     }
+});
+
+// 로그아웃 하기
+let logoutbtn=document.querySelector("#logoutbtn");
+logoutbtn?.addEventListener('click', ()=>{
+   location.href="/join/logout";
 });
