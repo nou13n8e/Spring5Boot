d8dc7512bcd4969efb6da694790327fc3304f116
nou <nou13n8e@gmail.com>
2023-07-12 10:25:42 +0900

    Board2 테이블 생성

--- spring5boot.sql
+++ spring5boot.sql
@@ -17,3 +17,28 @@ insert into member2 (userid, passwd, name, email, zipcode, add1, add2, phone)
 values ('abc123', '987xyz', 'abc123', 'abc123@987xyz', '123-456', '서울 관악구', '블라블라', '123-4567-8910');
 
 select * from member2;
+
+-- board
+create table board2 (
+    bno         int             auto_increment,
+    title       varchar(100)    not null,
+    userid      varchar(18)     not null,
+    regdate     datetime        default current_timestamp,
+    thumbs      int             default 0,
+    views       int             default 0,
+    contents    text            not null,
+    ipaddr      varchar(15)     not null,
+    primary key (bno)
+    -- foreign key (userid) references member2(userid)
+);
+
+alter table board2 add constraint fkuid foreign key(userid) references member2(userid);
+
+insert into board2 (title, userid, contents, ipaddr)
+values ('3 값이 필요한데, 1을(를) 가져왔습니다.', 'abc123', '3 값이 필요한데, 1을(를) 가져왔습니다.', '127.815.21.10');
+insert into board2 (title, userid, contents, ipaddr)
+values ('빌드가 731ms에서 성공적으로 완료되었습니다! (1분 전)', 'abc123a', '빌드가 731ms에서 성공적으로 완료되었습니다! (1분 전)', '168.15.12.21');
+insert into board2 (title, userid, contents, ipaddr)
+values ('모든 파일이 최신 상태입니다!', 'abc123c', '모든 파일이 최신 상태입니다!', '115.92.164.155');
+
+select count(userid) cnt, ceil(count(userid) / 25) from board2;
