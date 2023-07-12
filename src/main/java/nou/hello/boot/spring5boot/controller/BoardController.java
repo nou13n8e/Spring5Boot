f184858cac8c8490bd8c814d1df6d6f1fe55d6a4
nou <nou13n8e@gmail.com>
2023-07-12 15:39:00 +0900

    본문 보기 : 본문 보기 구현

--- src/main/java/nou/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/nou/hello/boot/spring5boot/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,10 +20,24 @@ public class BoardController {
     final BoardService bsrv;
 
     Logger logger= LogManager.getLogger(IndexController.class);
-    @GetMapping("/list")
-    public String list(Model m, Integer cpg) {
+    @GetMapping("/list/{cpg}")
+    public String list(Model m, @PathVariable Integer cpg) {
         logger.info("board list 호출!");
         m.addAttribute("board", bsrv.readBoard(cpg));
+        m.addAttribute("cpg", cpg);
         return "/board/list";
+    }
+
+    @GetMapping("/view/{bno}")
+    public String view(Model m, @PathVariable String bno) {
+        logger.info("board view 호출!");
+        m.addAttribute("view", bsrv.readOneBoard(bno));
+        return "/board/view";
+    }
+
+    @GetMapping("/write")
+    public String write() {
+        logger.info("board write 호출!");
+        return "/board/write";
     }
 }
