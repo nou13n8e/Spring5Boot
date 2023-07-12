bb4d8a815c896e054d9c7a696d2aeaba84028449
nou <nou13n8e@gmail.com>
2023-07-12 10:26:08 +0900

    Board Mapper 생성

---
+++ src/main/java/nou/hello/boot/spring5boot/mybatis/BoardMapper.java
@@ -0,0 +1,15 @@
+package nou.hello.boot.spring5boot.mybatis;
+
+import nou.hello.boot.spring5boot.model.Board;
+import nou.hello.boot.spring5boot.model.Member;
+import nou.hello.boot.spring5boot.model.Zipcode;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BoardMapper {
+    int insertBoard(Board b);
+    List<Board> selectBoard(int stnum);
+    Board selectOneBoard(String bno);
+}
