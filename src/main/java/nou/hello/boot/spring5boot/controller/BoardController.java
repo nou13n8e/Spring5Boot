8023dc3d7b72aabfc100e7d45f7a553e86e76749
nou <nou13n8e@gmail.com>
2023-07-12 11:02:13 +0900

    게시판 : BoardController 작성

--- src/main/java/nou/hello/boot/spring5boot/controller/BoardController.java
+++ src/main/java/nou/hello/boot/spring5boot/controller/BoardController.java
@@ -1,19 +1,28 @@
 package nou.hello.boot.spring5boot.controller;
 
+import lombok.RequiredArgsConstructor;
+import nou.hello.boot.spring5boot.service.BoardService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
+
+    @Autowired
+    final BoardService bsrv;
 
     Logger logger= LogManager.getLogger(IndexController.class);
     @GetMapping("/list")
-    public String list() {
+    public String list(Model m, int cpg) {
         logger.info("board list 호출!");
+        m.addAttribute("board", bsrv.readBoard(cpg));
         return "/board/list";
     }
 }
