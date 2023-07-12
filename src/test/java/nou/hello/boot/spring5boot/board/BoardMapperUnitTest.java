5fbdbf7fa792980c5355d782a76808868082f1cd
nou <nou13n8e@gmail.com>
2023-07-12 10:26:18 +0900

    Board Mapper 단위 테스트

---
+++ src/test/java/nou/hello/boot/spring5boot/board/BoardMapperUnitTest.java
@@ -0,0 +1,49 @@
+package nou.hello.boot.spring5boot.board;
+
+import nou.hello.boot.spring5boot.model.Board;
+import nou.hello.boot.spring5boot.mybatis.BoardMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import java.sql.SQLOutput;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BoardMapperUnitTest {
+    @Autowired
+    private BoardMapper boardMapper;
+
+    @Test
+    @DisplayName("BoardMapper insert Test")
+    void insertBoard() {
+        Board b=new Board(null, "", "abc123", null, "", "", "", "");
+        int result=boardMapper.insertBoard(b);
+        System.out.println(result);
+        assertEquals(result,1);
+    }
+
+    @Test
+    @DisplayName("BoardMapper selectBoard Test")
+    void selectBoard() {
+        int stnum=1;
+        List<Board> results=boardMapper.selectBoard(stnum);
+        System.out.println(results);
+        assertNotNull(results);
+    }
+
+    @Test
+    @DisplayName("BoardMapper selectOneBoard Test")
+    void selectOneBoard() {
+        String bno="300";
+        Board result=boardMapper.selectOneBoard(bno);
+        System.out.println(result);
+        assertNotNull(result);
+    }
+}
