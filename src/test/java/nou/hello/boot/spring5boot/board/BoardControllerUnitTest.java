a437c4afac253168706540b7417fc12402338f40
nou <nou13n8e@gmail.com>
2023-07-12 11:02:20 +0900

    게시판 : BoardController 단위 테스트

---
+++ src/test/java/nou/hello/boot/spring5boot/board/BoardControllerUnitTest.java
@@ -0,0 +1,36 @@
+package nou.hello.boot.spring5boot.board;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class BoardControllerUnitTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("BoardController save Test")
+    @Transactional
+    void readBoard() throws Exception {
+        mvc.perform(post("/board/list")
+                .param("cpg", "1"))
+                .andExpect(status().is4xxClientError())
+                .andReturn();
+    }
+
+}
