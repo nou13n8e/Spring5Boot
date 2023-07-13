dd3d36cd483ad4e4446d21d9a386cf7c863d6b4f
nou <nou13n8e@gmail.com>
2023-07-13 09:50:30 +0900

    게시판 : write js 작성

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -248,9 +248,27 @@ modbdbtn?.addEventListener('click', ()=>{
 });
 rmvbdbtn?.addEventListener('click', ()=>{
     if(confirm("글을 삭제하시겠습니까?")) {
-        alert("지원되지 않는 기능입니다.")
+        location.href="/board/list/1"
     }
 });
 lstbdbtn?.addEventListener('click', ()=>{
     location.href="/board/list/1"
+});
+
+
+// 글쓰기
+let wrtbdbtn=document.querySelector("#wrtbdbtn");
+let bdfrm=document.querySelector("#bdfrm");
+wrtbdbtn?.addEventListener('click', ()=>{
+    if(bdfrm.title.value==="") {
+        alert("제목을 입력해주세요.")
+    } else if (bdfrm.contents.value==="") {
+        alert("본문을 입력해주세요.")
+    } else if(grecaptcha.getResponse() === "") {
+        alert("자동입력방지를 해주세요.");
+    } else {
+        bdfrm.method = "post";
+        bdfrm.submit();
+        alert("작성이 완료되었습니다.")
+    }
 });
