6627132ddaa9cb68cd6a343ad429ac6ecac7b1ad
nou <nou13n8e@gmail.com>
2023-07-14 18:05:26 +0900

    자료실 : PdsDAO insertPdsAttach 구현

--- src/main/java/nou/hello/boot/spring5boot/dao/PdsDAO.java
+++ src/main/java/nou/hello/boot/spring5boot/dao/PdsDAO.java
@@ -1,7 +1,10 @@
 package nou.hello.boot.spring5boot.dao;
 
 import nou.hello.boot.spring5boot.model.Pds;
+import nou.hello.boot.spring5boot.model.PdsAttach;
 
 public interface PdsDAO {
     int insertPds(Pds p);
+
+    int insertPdsAttach(PdsAttach pa);
 }
