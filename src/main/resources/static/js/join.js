06f4b7a0b36b864eaa755bccaa72f754de79f13f
nou <nou13n8e@gmail.com>
2023-07-19 14:07:00 +0900

    자료실 : join.js에서 login.js 추출 - 로그인 여부에 따라 개별 메세지 출력

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -205,28 +205,6 @@ startbtn?.addEventListener('click', ()=>{
 // 회원가입으로 이동하기 joinbtn
 let joinbtn=document.querySelector("#joinbtn");
 joinbtn?.addEventListener('click', ()=> {
-   location.href="/join/agree"
+    location.href="/join/agree"
 });
 
-// 로그인 하기
-let loginbtn=document.querySelector("#loginbtn");
-let lgnuid=document.querySelector("#userid");
-let lgnpwd=document.querySelector("#passwd");
-let lgnfrm=document.querySelector("#lgnfrm");
-loginbtn?.addEventListener('click', ()=>{
-    if(lgnuid.value==="") {
-        alert("아이디를 입력해주세요.")
-    } else if (lgnpwd.value==="") {
-        alert("비밀번호를 입력해주세요.")
-    } else {
-        lgnfrm.method="post";
-        lgnfrm.action="/join/login";
-        lgnfrm.submit();
-    }
-});
-
-// 로그아웃 하기
-let logoutbtn=document.querySelector("#logoutbtn");
-logoutbtn?.addEventListener('click', ()=>{
-   location.href="/join/logout";
-});
