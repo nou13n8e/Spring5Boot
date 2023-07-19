e166ba38effe90e9a17e5baa2e8e725a46fee43a
nou <nou13n8e@gmail.com>
2023-07-19 14:01:18 +0900

    자료실 : newPdsComment, readPdsComment, newPdsReply 구현

--- src/main/java/nou/hello/boot/spring5boot/service/PdsService.java
+++ src/main/java/nou/hello/boot/spring5boot/service/PdsService.java
@@ -1,6 +1,7 @@
 package nou.hello.boot.spring5boot.service;
 
 import nou.hello.boot.spring5boot.model.Pds;
+import nou.hello.boot.spring5boot.model.PdsComment;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -8,16 +9,15 @@ import java.util.Map;
 
 public interface PdsService {
     int newPds(Pds p);
-
     boolean newPdsAttach(MultipartFile attach, int pno);
-
     List<Pds> readPds(Integer cpg);
-
     int countAllPds();
-
     Pds readOnePds(String pno);
-
     String readOnePdsAttach(String pno);
-
     Map<String, Object> getHeaderResource(String fname);
+
+    boolean newPdsComment(PdsComment pc);
+    List<PdsComment> readPdsComment(String pno);
+
+    boolean newPdsReply(PdsComment pc);
 }
--- src/main/java/nou/hello/boot/spring5boot/service/PdsServiceImpl.java
+++ src/main/java/nou/hello/boot/spring5boot/service/PdsServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import nou.hello.boot.spring5boot.dao.PdsDAO;
 import nou.hello.boot.spring5boot.model.Pds;
 import nou.hello.boot.spring5boot.model.PdsAttach;
+import nou.hello.boot.spring5boot.model.PdsComment;
 import nou.hello.boot.spring5boot.utils.PdsUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,7 +24,6 @@ public class PdsServiceImpl implements PdsService {
     public int newPds(Pds p) {
         return pdao.insertPds(p);
     }
-
     @Override
     public boolean newPdsAttach(MultipartFile attach, int pno) {
         // 1 첨부파일을 지정한 위치에 저장 후 정보를 알아내기
@@ -32,29 +32,24 @@ public class PdsServiceImpl implements PdsService {
         // pa.setPno(String.valueOf(pno)) 위와 같이 작성해도 되지만 단순하게 작성
         pa.setPno(pno + "");
 
-
         // 2 첨부파일에 대한 정보를 DB에 저장
         int pacnt=pdao.insertPdsAttach(pa);
 
         return (pacnt > 0) ? true : false;
     }
-
     @Override
     public List<Pds> readPds(Integer cpg) {
         int stnum=(cpg-1)*25;
         return pdao.selectPds(stnum);
     }
-
     @Override
     public int countAllPds() {
         return pdao.countAllPds();
     }
-
     @Override
     public Pds readOnePds(String pno) {
         return pdao.selectOnePds(pno);
     }
-
     @Override
     public String readOnePdsAttach(String pno) {
 
@@ -62,7 +57,6 @@ public class PdsServiceImpl implements PdsService {
 
         return pa.getFname();
     }
-
     @Override
     public Map<String, Object> getHeaderResource(String fname) {
         Map<String, Object> objs = new HashMap<>();
@@ -70,5 +64,20 @@ public class PdsServiceImpl implements PdsService {
         objs.put("header", pdsUtils.getHeader(fname));
         objs.put("resource", pdsUtils.getResource(fname));
         return objs;
+    }
+
+    @Override
+    public boolean newPdsComment(PdsComment pc) {
+        return (pdao.insertPdsComment(pc) > 0) ? true : false;
+    }
+
+    @Override
+    public List<PdsComment> readPdsComment(String pno) {
+        return pdao.selectPdsComment(pno);
+    }
+
+    @Override
+    public boolean newPdsReply(PdsComment pc) {
+        return (pdao.insertPdsReply(pc) > 0) ? true : false;
     }
 }
